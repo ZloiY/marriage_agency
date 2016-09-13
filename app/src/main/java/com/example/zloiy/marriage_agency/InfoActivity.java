d1dd2e655a86a89374b1469e853aeeacab795c8e
ZloiY <NoRFolf1000@mail.ru>
2016-09-13 21:02:02 +0300

    test version

---
+++ app/src/main/java/com/example/zloiy/marriage_agency/InfoActivity.java
@@ -0,0 +1,16 @@
+package com.example.zloiy.marriage_agency;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ZloiY on 13-Sep-16.
+ */
+public class InfoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.info_activity);
+    }
+}
--- app/src/main/java/com/example/zloiy/marriage_agency/MainActivity.java
+++ app/src/main/java/com/example/zloiy/marriage_agency/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.zloiy.marriage_agency;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView list = (ListView) findViewById(R.id.list_view);
+        String[] agency = {"marriage_agency"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, agency);
+        adapter.setNotifyOnChange(true);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, InfoActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
     }
 }
