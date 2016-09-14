c2d8c25f5e906b8656379ce632ce32b897c50674
ZloiY <NoRFolf1000@mail.ru>
2016-09-14 13:00:17 +0300

    work begin

---
+++ app/src/main/java/com/example/zloiy/marriage_agency/DBColumns.java
@@ -0,0 +1,16 @@
+package com.example.zloiy.marriage_agency;
+
+/**
+ * Created by ZloiY on 14-Sep-16.
+ */
+public interface DBColumns {
+    public final String ID = "_id";
+    public final String NAME = "name";
+    public final String TELEPHONE_ID = "telephone_id";
+    public final String EMAIL_ID = "email_id";
+    public final String WEBSITE_ID = "website_id";
+    public final String AGENCY_TABLE = "agency_table";
+    public final String WEBSITE_TABLE = "website_table";
+    public final String TELEPHONE_TABLE = "telephone_table";
+    public final String EMAIL_TABLE = "email_table";
+}
---
+++ app/src/main/java/com/example/zloiy/marriage_agency/DBModel.java
@@ -0,0 +1,31 @@
+package com.example.zloiy.marriage_agency;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by ZloiY on 14-Sep-16.
+ */
+public class DBModel extends SQLiteOpenHelper implements DBColumns{
+    public static final String DB_NAME = "agency.db";
+    public static final int DB_VERSION = 1;
+    public static final String CREATE_AGENCY = "create table" + AGENCY_TABLE + "(" + ID + "primary key autoincrement" + NAME + "text not null" + TELEPHONE_ID + "int not null" +
+            WEBSITE_ID + "int not null" + EMAIL_ID + "int not null" + ")";
+    public static final String CREATE_EMAIL = "create table" + EMAIL_TABLE + "(" + ID + "primary key autoincrement" + NAME + "text not null" + ")";
+    public static final String CREATE_WEBSITE = "create table" + WEBSITE_TABLE + "(" + ID + "primary key autoincrement" + NAME + "text not null" + ")";
+    public static final String CREATE_TELEPHONE = "create table" + TELEPHONE_TABLE + "(" + ID + "primary key autoincrement" + NAME + "text not null" + ")";
+    public DBModel(Context context){super(context, DB_NAME, null, DB_VERSION);}
+    public void onCreate (SQLiteDatabase database){
+        database.execSQL(CREATE_AGENCY);
+        database.execSQL(CREATE_EMAIL);
+        database.execSQL(CREATE_WEBSITE);
+        database.execSQL(CREATE_TELEPHONE);
+    }
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop if exists" + CREATE_AGENCY);
+        db.execSQL("drop if exists" + CREATE_EMAIL);
+        db.execSQL("drop if exists" + CREATE_WEBSITE);
+        db.execSQL("drop if exists" + CREATE_TELEPHONE);
+    }
+}
