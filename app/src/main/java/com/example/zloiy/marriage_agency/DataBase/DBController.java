eeddf262b70590a89673747283f43af8e241675d
ZloiY <NoRFolf1000@mail.ru>
2016-09-27 21:12:07 +0300

    copying db

--- app/src/main/java/com/example/zloiy/marriage_agency/DataBase/DBController.java
+++ app/src/main/java/com/example/zloiy/marriage_agency/DataBase/DBController.java
@@ -6,6 +6,8 @@ import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 
+import java.io.IOException;
+
 
 /**
  * Created by ZloiY on 14-Sep-16.
@@ -17,6 +19,16 @@ public class DBController implements DBColumns {
     public DBController(Context context){this.context = context;}
     public DBController open() throws SQLException{
         dbModel = new DBModel(context);
+        try {
+            dbModel.createDatabase();
+        } catch (IOException ioe) {
+            throw new Error("Unable to create database");
+        }
+        try {
+            dbModel.openDataBase();
+        }catch(SQLException sqle){
+            throw sqle;
+        }
         database = dbModel.getWritableDatabase();
         return this;
     }
--- app/src/main/java/com/example/zloiy/marriage_agency/DataBase/DBModel.java
+++ app/src/main/java/com/example/zloiy/marriage_agency/DataBase/DBModel.java
@@ -2,15 +2,24 @@ package com.example.zloiy.marriage_agency.DataBase;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import com.example.zloiy.marriage_agency.DataBase.DBColumns;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.sql.SQLException;
 
 /**
  * Created by ZloiY on 14-Sep-16.
  */
 public class DBModel extends SQLiteOpenHelper implements DBColumns {
     public static final String DB_NAME = "agency.db";
+    public static final String DB_PATH = "/data/data/com.example.zloiy.marriage_agency/databases/";
     public static final int DB_VERSION = 1;
     public static final String CREATE_AGENCY = "create table " + AGENCY_TABLE + "(" + ID + " integer primary key autoincrement, " + NAME + " text not null, " + TELEPHONE_ID + " integer not null, " +
             WEBSITE_ID + " integer not null, " + EMAIL_ID + " integer not null, " + STREET_ID + " text not null)";
@@ -18,13 +27,60 @@ public class DBModel extends SQLiteOpenHelper implements DBColumns {
     public static final String CREATE_WEBSITE = "create table " + WEBSITE_TABLE + "(" + ID + " integer primary key autoincrement, " + NAME + " text not null" + ")";
     public static final String CREATE_TELEPHONE = "create table " + TELEPHONE_TABLE + "(" + ID + " integer primary key autoincrement, " + NAME + " int not null" + ")";
     public static final String CREATE_STREET = "create table " + STREET_TABLE + "(" + ID + " integer primary key autoincrement, " + NAME + " text not null" + ")";
-    public DBModel(Context context){super(context, DB_NAME, null, DB_VERSION);}
+    private final Context myContext;
+    private SQLiteDatabase myDataBase;
+    public DBModel(Context context){
+        super(context, DB_NAME, null, DB_VERSION);
+        myContext = context;}
+    public void  createDatabase()throws IOException{
+        boolean dbExist = checkDataBase();
+        if(!dbExist){
+            this.getReadableDatabase();
+            try{
+                copyDataBase();
+            }catch (IOException e){
+                throw new Error("Error copying database!");
+            }
+        }
+    }
+    private boolean checkDataBase(){
+        SQLiteDatabase checkDB = null;
+        try{
+            String myPath = DB_PATH + DB_NAME;
+            checkDB = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+        }catch (SQLiteException e){
+            Log.e("Log", "DataBase didn't create.");
+        }
+        if(checkDB!=null)checkDB.close();
+        return checkDB!=null?true:false;
+    }
+    private void copyDataBase()throws IOException{
+        InputStream myInput = myContext.getAssets().open(DB_NAME);
+        String outFileName = DB_PATH + DB_NAME;
+        OutputStream myOutPut = new FileOutputStream(outFileName);
+        byte[] buffer = new byte[1024];
+        int lentgh;
+        while ((lentgh = myInput.read(buffer))>0){
+            myOutPut.write(buffer, 0, lentgh);
+        }
+        myOutPut.flush();
+        myOutPut.close();
+        myInput.close();
+    }
+    public void openDataBase() {
+        String myPath = DB_PATH + DB_NAME;
+        myDataBase = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+    }
+    public synchronized void close(){
+        if(myDataBase!=null)myDataBase.close();
+        super.close();
+    }
     public void onCreate (SQLiteDatabase database){
-        database.execSQL(CREATE_AGENCY);
+        /*database.execSQL(CREATE_AGENCY);
         database.execSQL(CREATE_EMAIL);
         database.execSQL(CREATE_WEBSITE);
         database.execSQL(CREATE_TELEPHONE);
-        database.execSQL(CREATE_STREET);
+        database.execSQL(CREATE_STREET);*/
     }
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("drop if exists" + CREATE_AGENCY);
